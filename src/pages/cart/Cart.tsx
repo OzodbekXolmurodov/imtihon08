196e9398d55d8c177b269362145a62428029f89d
OzodbekXolmurodov <ozodbekxolmurodov08@gmail.com>
2025-01-20 20:45:27 +0500

    final

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,3 +1,4 @@
+import { AiFillDelete } from "react-icons/ai";
 import { Product } from "../../api/Api";
 import rasim1 from "./../../assets/minimal-49-512.webp";
 
@@ -12,19 +13,37 @@ const Cart = () => {
 
   return (
     <div className="contianer">
-      <div>
+      <h2 className="text-[40px] font-bold mt-10 mb-10"> Your cart</h2>
+      <div className="rounded-xl border-[1px] p-8 w-[600px]">
         {cart.length === 0 ? (
           <div className="flex justify-center">
             <img src={rasim1} alt="" />
           </div>
         ) : (
           cart.map((product: Product) => (
-            <div key={product.id} className="cart-item">
-              <img src={product.urls[0]} alt={product.name} />
+            <div
+              key={product.id}
+              className="cart-item flex items-center gap-4 mb-6"
+            >
+              <img
+                className="w-[124px] rounded-2xl"
+                src={product.urls[0]}
+                alt={product.name}
+              />
               <div>
-                <h3>{product.name}</h3>
-                <p>Price: ${product.desprice}</p>
-                <button onClick={() => handleRemove(product.id)}>Remove</button>
+                <div className="flex items-baseline justify-between gap-[100px] ">
+                  <h3 className="text-[20px] font-bold mb-2">{product.name}</h3>
+                  <button onClick={() => handleRemove(product.id)}>
+                    <AiFillDelete className="text-xl text-red-700" />
+                  </button>
+                </div>
+                <p className="font-bold">
+                  size : <span className="font-light text-sm">Large</span>
+                </p>
+                <p className="font-bold mb-1">
+                  color : <span className="font-light text-sm">White</span>
+                </p>
+                <p className="text-[24px]">${product.desprice}</p>
               </div>
             </div>
           ))
